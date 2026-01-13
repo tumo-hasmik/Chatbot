6964ebd19279e3a38050b5f0849ed6388acd176d
Student <hasmik.babayan1@tumo.org>
2026-01-13 19:24:29 +0400

    dfghjk

--- example.py
+++ example.py
@@ -6,6 +6,16 @@ client = OpenAI()
 
 user_prompt = input("Enter your prompt!!")
 
+input_messages = [
+    {"role": "system", "content": 
+    "You are a helpful assistant that answers clearly."
+    "You are specialized in music and you can answer to every question about music."
+    "Avoid unnecessary jargon. "
+    "The users can sing songs, write lyrics, etc and you will be able to play the continuing of the song."
+    },
+    {"role": "user", "content": "Explain recursion in simple terms."}
+]
+
 response = client.responses.create(
     model = "gpt-5-nano",
     input = user_prompt
